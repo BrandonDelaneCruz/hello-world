97db79ca420dc9d83d0967595f908f104ffdd124
BrandonCruz <brandondelanecruz@gmail.com>
2024-07-18 21:55:04 -0500

    feat: add stopwatch

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Press to start.");
+
+var userSaidStart = Console.ReadLine();
+
+var startTime = DateTime.Now;
+
+Console.Clear();
+
+Console.WriteLine("Press to end.");
+
+var userSaidStop = Console.ReadLine();
+
+var endTime = DateTime.Now;
+
+var howLongDidHeWaitToPressTheButton = endTime - startTime;
+
+Console.Clear();
+
+Console.WriteLine(howLongDidHeWaitToPressTheButton);
